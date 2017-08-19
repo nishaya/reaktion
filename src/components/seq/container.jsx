bf85f03aa0935e245ef46730ce62c97cff5e85f8
nishaya <nishaya@users.noreply.github.com>
2017-08-19 22:07:37 +0900

    add initial tracks

--- src/components/seq/container.jsx
+++ src/components/seq/container.jsx
@@ -10,28 +10,34 @@ const Button = RaisedButton
 const MAX_TRACKS = 4
 
 type State = {
-  numTracks: number,
   tracks: Array<any>,
 }
 
+type Props = {
+  defaultTracks: number,
+}
+
 export default class SeqContainer extends Component {
+  static defaultProps = {
+    defaultTracks: 2,
+  }
+
   state: State = {
-    numTracks: 2,
     tracks: [],
   }
 
-  renderTracks() {
-    const tracks = []
-    for (let i = 0; i < this.state.numTracks; i += 1) {
-      tracks.push(<Track key={`track_${i}`} />)
-    }
-    return tracks
+  componentDidMount() {
+    this.addTrack(this.props.defaultTracks)
   }
 
-  addTrack() {
+  props: Props
+
+  addTrack(num: number = 1) {
     const { tracks } = this.state
-    if (tracks.length < MAX_TRACKS) {
-      tracks.push(<Track key={`track_${tracks.length}`} />)
+    if (tracks.length + num <= MAX_TRACKS) {
+      for (let i = 0; i < num; i += 1) {
+        tracks.push(<Track key={`track_${tracks.length}`} />)
+      }
     }
     this.setState({ tracks })
   }
