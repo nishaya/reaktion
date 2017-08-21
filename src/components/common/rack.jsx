666c79fa8e4c3f0ee5deaf01872e631492356042
nishaya <nishaya@users.noreply.github.com>
2017-08-22 08:58:35 +0900

    modify style

--- src/components/common/rack.jsx
+++ src/components/common/rack.jsx
@@ -3,10 +3,11 @@
 import styled from 'styled-components'
 
 const Rack = styled.div`
-  padding: 4px;
-  border: 2px solid #999;
+  margin: 8px;
+  padding: 8px;
   background-color: ${props => props.theme.bgColor};
   display: block;
+  box-shadow:2px 2px 2px 0px rgba(0,0,0,0.39);
 
   h2 {
     font-size: 16px;
