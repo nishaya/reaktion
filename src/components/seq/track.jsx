2bd8819ae489e16a968e75cd84acde463b5c556f
nishaya <nishaya@users.noreply.github.com>
2017-08-21 14:37:19 +0900

    onSynthReady

--- src/components/seq/track.jsx
+++ src/components/seq/track.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import Rack from 'components/common/rack'
 import type { Steps } from 'types/step'
+import SimpleSynth from 'synth/simple_synth'
 import Pattern from './pattern'
 
 type Props = {
   trackId: string,
   onTrackFixed: (steps: Steps, trackId: string) => any,
+  onSynthReady: (synth: SimpleSynth) => void,
 }
 
 export default class Track extends Component<any, any, Props> {
   static defaultProps = {
     trackId: 'not set',
     onTrackFixed: (steps: Steps, trackId: string) => console.log(`track fixed(Track #${trackId})`, steps),
+    onSynthReady: (synth: SimpleSynth) => console.log('Track.onSynthReady', synth),
   }
 
+  componentWillMount() {
+    this.synth = new SimpleSynth()
+    this.props.onSynthReady(this.synth)
+  }
+
+  synth: SimpleSynth
   props: Props
 
   render() {
