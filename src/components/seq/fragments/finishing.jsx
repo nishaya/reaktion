94efadcee0c2f0c5c0860aa9b2e61244b77dd505
nishaya <nishaya@users.noreply.github.com>
2017-08-22 08:48:36 +0900

    fill notes

--- src/components/seq/fragments/finishing.jsx
+++ src/components/seq/fragments/finishing.jsx
@@ -13,6 +13,7 @@ type Props = {
   patternLength: number,
   mute: boolean,
   solo: boolean,
+  fill: boolean,
 }
 
 type State = {
@@ -26,6 +27,7 @@ export default class Finishing extends Component<any, Props, State> {
     patternLength: 16,
     mute: false,
     solo: false,
+    fill: true,
   }
 
   state: State = {
@@ -43,9 +45,25 @@ export default class Finishing extends Component<any, Props, State> {
   props: Props
 
   transform(props: Props) {
-    const { steps, patternLength } = props
+    const { steps, patternLength, fill } = props
+    const oldList = steps.list
+    const oldLength = steps.length
     const newList = steps.list.slice(0)
-    const newSteps = { length: patternLength, list: newList }
+    if (fill && oldLength > 0 && patternLength > oldLength) {
+      const repeatCount = Math.ceil(patternLength / oldLength)
+      console.log('repeat count', repeatCount)
+      for (let i = 1; i < repeatCount; i += 1) {
+        newList.push(
+          ...oldList.map(s => (
+            { ...s, position: s.position + (i * oldLength) }
+          )),
+        )
+      }
+    }
+    const newSteps = {
+      length: patternLength,
+      list: newList.filter(s => s.position < patternLength),
+    }
     this.props.onChange(newSteps)
     this.setState({ steps: newSteps })
   }
