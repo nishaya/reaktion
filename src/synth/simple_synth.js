554727bba8bbeb7b3f2f12142980e95cd5a9e009
nishaya <nishaya@users.noreply.github.com>
2017-08-21 10:12:03 +0900

    add simple synth

---
+++ src/synth/simple_synth.js
@@ -0,0 +1,38 @@
+// @flow
+
+const ctx = new window.AudioContext()
+
+type PlayOption = {
+  offset?: number,
+  duration?: number,
+  velocity?: number,
+}
+
+const defaultPlayOption: PlayOption = {
+  offset: 0,
+  duration: 0.2,
+  velocity: 100,
+}
+
+export default class SimpleSynth {
+  waveform: string = 'square'
+
+  play(note: number, playOption: PlayOption = {}) {
+    const { offset, duration } = { ...defaultPlayOption, ...playOption }
+    const startTime = ctx.currentTime + offset
+    const frequency = 440 * (2 ** ((note - 69) / 12))
+    console.log(`Synth.scheduled, offset: ${offset}, startTime: ${startTime}, note: ${note}, ${frequency}`)
+    const osc = ctx.createOscillator()
+    const gain = ctx.createGain()
+
+    gain.gain.setValueAtTime(1, startTime)
+    gain.gain.setValueAtTime(1, startTime + (duration * 0.7))
+    gain.gain.exponentialRampToValueAtTime(0.01, startTime + duration)
+    osc.type = this.waveform
+    osc.frequency.value = frequency
+    osc.connect(gain)
+    gain.connect(ctx.destination)
+    osc.start(startTime)
+    osc.stop(startTime + duration)
+  }
+}
