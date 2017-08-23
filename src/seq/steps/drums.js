a474273d504182a7862a0cb908f610ec8df1a63f
nishaya <nishaya@users.noreply.github.com>
2017-08-23 19:33:28 +0900

    add snare

--- src/seq/steps/drums.js
+++ src/seq/steps/drums.js
@@ -4,6 +4,7 @@ import type { Steps, Step } from 'types/step'
 
 const kick: Step = { position: 0, note: 0, velocity: 100 }
 const chh: Step = { position: 0, note: 6, velocity: 100 }
+const snare: Step = { position: 0, note: 2, velocity: 100 }
 
 export const basicBeats: Steps = {
   length: 16,
@@ -11,10 +12,12 @@ export const basicBeats: Steps = {
     { ...kick, position: 0 },
     { ...chh, position: 2 },
     { ...kick, position: 4 },
+    { ...snare, position: 4 },
     { ...chh, position: 6 },
     { ...kick, position: 8 },
     { ...chh, position: 10 },
     { ...kick, position: 12 },
+    { ...snare, position: 12 },
     { ...chh, position: 14 },
   ],
 }
