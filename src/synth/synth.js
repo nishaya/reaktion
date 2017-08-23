fe04320fcd868d465d8ae17fa1ef6f8f8d7a9c7e
nishaya <nishaya@users.noreply.github.com>
2017-08-24 00:08:39 +0900

    playback duration

--- src/synth/synth.js
+++ src/synth/synth.js
@@ -147,12 +147,12 @@ export default class Synth {
   }
 
   playSample(tone: Tone) {
-    const { note, offset } = { ...defaultTone, ...tone }
+    const { note, offset, duration } = { ...defaultTone, ...tone }
     const startTime = ctx.currentTime + offset
     const playbackRate = (2 ** ((note - 69) / 12))
     console.log(`Sample.scheduled, offset: ${offset}, startTime: ${startTime}, note: ${note}, rate: ${playbackRate}`)
     // const volume = velocity / 127 * 0.7
     const sampler = new PlaybackSampler(this.sampleSynthPreset.sample)
-    sampler.play({ when: offset, playbackRate })
+    sampler.play({ when: offset, playbackRate, duration, loop: true })
   }
 }
