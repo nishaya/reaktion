edb7e049362b0c8098626e0b77a6d204cecdec1a
nishaya <nishaya@users.noreply.github.com>
2017-08-26 07:57:46 +0900

    improve editor

--- src/components/seq/fragments/steps_editor.jsx
+++ src/components/seq/fragments/steps_editor.jsx
@@ -11,6 +11,10 @@ type Props = {
   onRequestClose: () => void,
 }
 
+type State = {
+  editNotes: Array<number>,
+}
+
 export default class StepEdit extends Component {
   static defaultProps = {
     show: false,
@@ -20,9 +24,32 @@ export default class StepEdit extends Component {
   }
 
   props: Props
+  state: State = {
+    editNotes: [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71],
+  }
+
+  checkStep(position: number, note: number, checked: boolean) {
+    console.log('checkStep', position, note, checked)
+    const { onEdit, steps } = this.props
+    if (checked) {
+      const newSteps = steps.list.slice(0)
+      newSteps.push({
+        note,
+        position,
+        velocity: 100,
+        duration: 1.0,
+      })
+      onEdit({ ...steps, list: newSteps })
+    } else {
+      const newSteps = steps.list.filter(s => s.position !== position || s.note !== note)
+      console.log(newSteps)
+      onEdit({ ...steps, list: newSteps })
+    }
+  }
 
   render() {
-    const { show, steps, onEdit, onRequestClose } = this.props
+    const { show, steps, onRequestClose } = this.props
+    const { editNotes } = this.state
     return (<div>
       <Dialog
         open={show}
@@ -30,30 +57,20 @@ export default class StepEdit extends Component {
         title="edit steps"
       >
         <div style={{ width: '500px' }}>
-          {new Array(steps.length).fill(null).map((x, index) => {
-            const step = steps.list.find(s => s.position === index)
-            const checked = step !== undefined
-            const key = `e_${index}`
-            return (<Checkbox
-              key={key}
-              style={{ padding: 0, margin: 0, width: 30 }}
-              checked={checked}
-              onCheck={(e, v) => {
-                if (v) {
-                  const newSteps = steps.list.slice(0)
-                  newSteps.push({
-                    note: 64,
-                    position: index,
-                    velocity: 100,
-                    duration: 1.0,
-                  })
-                  onEdit({ ...steps, list: newSteps })
-                } else {
-                  const newSteps = steps.list.filter(s => s.position !== index)
-                  onEdit({ ...steps, list: newSteps })
-                }
-              }}
-            />)
+          {editNotes.map((en) => {
+            return new Array(steps.length).fill(null).map((x, index) => {
+              const step = steps.list.find(s => s.position === index)
+              const checked = step !== undefined
+              const key = `e_${index}`
+              return (<Checkbox
+                key={key}
+                style={{ padding: 0, margin: 0, width: 30 }}
+                checked={checked}
+                onCheck={(e, v) => {
+                  this.checkStep(index, 60, v)
+                }}
+              />)
+            })
           })}
         </div>
       </Dialog>
