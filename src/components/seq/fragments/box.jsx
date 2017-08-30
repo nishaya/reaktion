3e466b67627c3416b8426e30fc435427e8d6dec0
nishaya <nishaya@users.noreply.github.com>
2017-08-30 11:41:27 +0900

    modify fonts

--- src/components/seq/fragments/box.jsx
+++ src/components/seq/fragments/box.jsx
@@ -10,11 +10,17 @@ const Box = styled.div`
   display: inline-block;
   width: 148px;
   box-shadow:1px 1px 2px 1px rgba(0,0,0,0.39);
+  font-family: 'Roboto Mono', monospace;
+  font-size: 14px;
 
   h2 {
-    font-size: 16px;
+    font-size: 18px;
+    font-weight: 800;
     padding: 2px;
     margin: 2px;
+    .text {
+      vertical-align: top;
+    }
   }
 
   .control {
